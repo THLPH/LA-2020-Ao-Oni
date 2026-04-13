a4853f682cfc66c428721953a6fb75610c771fec
THLPH <94089638+THLPH@users.noreply.github.com>
2026-04-13 11:57:32 -0400

    Initialize session defaults and update variable shortcuts

--- init.php
+++ init.php
@@ -2,18 +2,36 @@
 session_start();
 
 // AUTHENTICATION GUARD
+
 if (!isset($_SESSION['username'])) {
     header("Location: login.php");
     exit();
 }
 
+// INITIALIZE SESSION DEFAULTS
+
+$_SESSION['empathy']    ??= 0;
+$_SESSION['ending']     ??= 1;
+$_SESSION['day']        ??= 1;
+$_SESSION['credits']    ??= 0;
+$_SESSION['weapon']     ??= 0;
+$_SESSION['cybernetic'] ??= 0;
+$_SESSION['manga']      ??= false;
+$_SESSION['class']      ??= 'UNASSIGNED';
+$_SESSION['attack']        ??= 0;
+$_SESSION['defense']        ??= 0;
+
 // GLOBAL VARIABLE SHORTCUTS
-$user =  $_SESSION['username'];
-$emp  = &$_SESSION['empathy']    ?? 0;
-$end  = &$_SESSION['ending']     ?? 1;
-$day  = &$_SESSION['day']        ?? 1;
-$cred = &$_SESSION['credits']    ?? 0;
-$wep  = &$_SESSION['weapon']     ?? 0;
-$cyb  = &$_SESSION['cybernetic'] ?? 0;
-$sec  = &$_SESSION['manga']      ?? false;
+
+$user  = $_SESSION['username'];
+$emp   = &$_SESSION['empathy'];
+$end   = &$_SESSION['ending'];
+$day   = &$_SESSION['day'];
+$cred  = &$_SESSION['credits'];
+$wep   = &$_SESSION['weapon'];
+$cyb   = &$_SESSION['cybernetic'];
+$sec   = &$_SESSION['manga'];
+$class = &$_SESSION['class'];
+$atk   = &$_SESSION['attack'];
+$def   = &$_SESSION['defense'];
 ?>
